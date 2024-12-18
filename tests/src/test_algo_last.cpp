5c97ae96ebbf54bbcb00fb9ef806517c2eb87c19
michael <mhernan88@gmail.com>
2024-12-18 17:32:32 -0600

    algo last tests

--- tests/src/test_algo_last.cpp
+++ tests/src/test_algo_last.cpp
@@ -1,4 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <armadillo>
 #include <memory>
 #include "algo_last.h"
@@ -25,10 +27,25 @@ TEST_CASE("Test LastRelimpAlgorithm.evaluate_column") {
 	std::unique_ptr<LastRelimpAlgorithm> ra(new LastRelimpAlgorithm);
 
 	auto cc = ra->evaluate_column(x, y, 0);
-	WARN(cc.get_lift());
-	WARN(cc.get_mean_with_column());
-	WARN(cc.get_mean_without_column());
-	REQUIRE(1==1);
+
+	auto mean_without = cc.get_mean_without_column();
+	REQUIRE_THAT(
+			mean_without,
+			Catch::Matchers::WithinRel(-2.985, 0.001)
+	);
+
+	auto mean_with = cc.get_mean_with_column();
+	REQUIRE_THAT(
+			mean_with, 
+			Catch::Matchers::WithinRel(0.5265, 0.001)
+	);
+
+	auto lift = cc.get_lift();
+	auto calculated_lift = mean_with - mean_without;
+	REQUIRE_THAT(
+			lift, 
+			Catch::Matchers::WithinRel(calculated_lift, 0.001)
+	);
 }
 
 TEST_CASE("Test LastRelimpAlgorithm.evaluate_columns") {
@@ -41,12 +58,12 @@ TEST_CASE("Test LastRelimpAlgorithm.evaluate_columns") {
 	arma::dvec y = data.col(2);
 
 	std::unique_ptr<LastRelimpAlgorithm> ra(new LastRelimpAlgorithm);
+
 	auto ccs = ra->evaluate_columns(x, y);
-	REQUIRE(ccs.size() > 0);
 	REQUIRE(ccs.size() == x.n_cols);
-	
+
 	for (ColumnContribution cc : ccs) {
-		WARN(cc.get_lift());
+		REQUIRE(cc.get_lift() != 0);
 	}
-	REQUIRE(1==1);
 }
+
