4a2a4ef84c0df5e29c0659e442346b4a0f11567e
michael <mhernan88@gmail.com>
2024-12-19 17:47:40 -0600

    moved smoke test logic to wrapper

--- smoke_test.R
+++ smoke_test.R
@@ -1,30 +1,6 @@
 library("relix")
 
-x <- as.matrix(mtcars)
-x_ok <- is.matrix(x)
-if (x_ok == FALSE) {
-	stop("x is not a matrix")
-}
-x_ok <- is.matrix(x) && is.numeric(x)
+fit <- lm("mpg ~ disp + hp + drat + hp * drat", data=mtcars)
+output <- relix(fit, "last")
+print(output)
 
-x_ok <- is.numeric(x)
-if (x_ok == FALSE) {
-	stop("x is not numeric")
-}
-
-y <- mtcars[,3]
-y_ok <- is.numeric(y)
-
-if (y_ok == FALSE) {
-	stop("y is not numeric")
-}
-
-print("X...")
-print(x)
-
-print("Y...")
-print(y)
-
-print("************************************************")
-
-print(relix_r(x, y))
