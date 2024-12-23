ab833b709cba62179ceefaa8fefe4ef921c1fbd2
michael <mhernan88@gmail.com>
2024-12-23 01:40:14 -0600

    using fixture instead

--- tests/src/test_relix.cpp
+++ tests/src/test_relix.cpp
@@ -4,23 +4,35 @@
 #include <armadillo>
 #include "relix.h"
 #include "algos.h"
+#include "fixtures/mtcars.h"
 
-TEST_CASE("Test Pipeline") {
-	arma::dmat data = arma::zeros(5, 4);
-	bool ok = data.load("../tests/data/sample1.csv");
+TEST_CASE_METHOD(MTCars, "Test-Pipeline-1") {
+	std::vector<std::string> x_labs = {"cyl", "disp", "hp", "drat"};
 
-	REQUIRE(ok);
-	REQUIRE(arma::accu(data) > 0);
-
-	arma::dmat x = data.cols(0, 1);
-	arma::dvec y = data.col(2);
-	
 	LastRelimpAlgorithm ra = LastRelimpAlgorithm();
-	std::vector<ColumnContribution> ccs =  relative_importance(x, y, ra);
+	std::vector<ColumnContribution> ccs = relative_importance(get_x(x_labs), get_y(), ra);
 
-	double last_lift = -9999;
 	for (ColumnContribution cc : ccs) {
-		REQUIRE(cc.get_lift() > last_lift);
-		last_lift = cc.get_lift();
+		WARN(cc.get_lift());
 	}
 }
+
+// TEST_CASE("Test Pipeline") {
+// 	arma::dmat data = arma::zeros(5, 4);
+// 	bool ok = data.load("../tests/data/sample1.csv");
+//
+// 	REQUIRE(ok);
+// 	REQUIRE(arma::accu(data) > 0);
+//
+// 	arma::dmat x = data.cols(0, 1);
+// 	arma::dvec y = data.col(2);
+// 	
+// 	LastRelimpAlgorithm ra = LastRelimpAlgorithm();
+// 	std::vector<ColumnContribution> ccs = relative_importance(x, y, ra);
+//
+// 	double last_lift = -9999;
+// 	for (ColumnContribution cc : ccs) {
+// 		REQUIRE(cc.get_lift() > last_lift);
+// 		last_lift = cc.get_lift();
+// 	}
+// }
