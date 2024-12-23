60e4fb1e0bd062cb6e2f6cd91c294e61bc9be05a
michael <mhernan88@gmail.com>
2024-12-23 15:10:32 -0600

    make basic_lm more universal

--- src/lm.cpp
+++ src/lm.cpp
@@ -7,19 +7,16 @@ arma::dmat add_intercept(const arma::dmat& x) {
 
 }
 
-Model basic_lm(
-                const arma::dmat& x,
-                const arma::dvec& y
-              ) {
-        arma::vec beta = arma::solve(x, y);
-        arma::vec y_pred = x * beta;
-        arma::vec residuals = y - y_pred;
+Model basic_lm(const arma::dmat& x, const arma::dvec& y) {
+    arma::vec beta = arma::solve(arma::trans(x) * x, arma::trans(x) * y);
+    arma::vec y_pred = x * beta;
+    arma::vec residuals = y - y_pred;
 
-        double ss_res = arma::accu(arma::square(residuals));
-        double ss_tot = arma::accu(arma::square(y - arma::mean(y)));
-        double r_squared = 1 - (ss_res / ss_tot);
+    double ss_res = arma::accu(arma::square(residuals));
+    double ss_tot = arma::accu(arma::square(y - arma::mean(y)));
+    
+    double r_squared = 1 - (ss_res / ss_tot);
 
-        return Model(
-                        beta, y_pred, residuals, r_squared
-                    );
+    return Model(beta, y_pred, residuals, r_squared);
 }
+
