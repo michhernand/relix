79816584f34f8b1137579c4c1992be1e49e4cd4d
michael <mhernan88@gmail.com>
2024-12-24 00:57:38 -0600

    removed unused stuff

--- src/relix.cpp
+++ src/relix.cpp
@@ -1,15 +1,5 @@
 #include <armadillo>
-#include <memory>
-#include <stdexcept>
 #include "algos.h"
-
-std::unique_ptr<RelimpAlgorithm> get_algo(std::string& algo) {
-	if (algo == "last") {
-		return std::make_unique<LastRelimpAlgorithm>();
-	}
-	throw std::invalid_argument("invalid algo");
-}
-
 
 arma::dvec relative_importance(
 		const arma::dmat& x, 
@@ -20,13 +10,3 @@ arma::dvec relative_importance(
 	return rsqs;
 }
 
-arma::dvec relative_importance(
-		const arma::dmat& x,
-		const arma::dmat& y,
-		std::string& algo
-) {
-
-	std::unique_ptr<RelimpAlgorithm> algoObj = get_algo(algo);
-	auto ccs = algoObj->evaluate_columns(x, y);
-	return ccs;
-}
