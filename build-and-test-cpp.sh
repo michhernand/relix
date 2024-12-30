a9ca0bc0ad1d7e8e69d165dc81a47b6654f85e95
michael <mhernan88@gmail.com>
2024-12-30 00:22:53 -0500

    removed unused scripts

--- build-and-test-cpp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sh ./clean.sh
-mkdir ./build
-cd build && cmake .. -DUSE_R_LIBS=OFF
-cmake --build . -- -j4
-./tests/relix_test
--- rebuild_cpp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-cmake --preset default -DUSE_R_LIBS=OFF && cmake --build ./build -- -j4
